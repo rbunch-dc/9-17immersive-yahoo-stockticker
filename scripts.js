1a4a05ff993353a477f14a07b6db42b3f63d7cdf
Robert Bunch <rob@digitalcrafts.com>
2017-10-11 14:09:51 -0400

    init repo

---
+++ scripts.js
@@ -0,0 +1,1 @@
+		var url = 'http://query.yahooapis.com/v1/public/yql?q=env%20%27store://datatables.org/alltableswithkeys%27;select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22goog%22)%0A%09%09&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json';
